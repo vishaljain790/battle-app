6649cf907eaa49d0b000e9584a560da4cdd31cd2
Vishal_team <Vishal.j@myteam11.com>
2018-08-28 21:12:50 +0530

    checking configuration with heroku for es6

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
 
+app.get('/', (req,res) => {
+    res.send('hey user!!');
+}); 
+
 //listening on port
 app.listen(port, () => {
     console.log(`server connected on port: ${port}`);
